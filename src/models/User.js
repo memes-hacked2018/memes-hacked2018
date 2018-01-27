d34339bce7b89ace2c7e0d55b9704f8f973f17e2
amalik4 <Adil_Malik@intuit.com>
2018-01-27 12:37:55 -0700

    user model

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+class User {
+
+    /**
+     * Constructs a new User object
+     * @param {String} name - the name of the user
+     */
+    constructor(name) {
+        this.name = name;
+        this.ratings = ratings;
+    }
+
+}
