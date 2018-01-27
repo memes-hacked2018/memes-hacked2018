46888ebf79b7e3e5d7cf37f17d03021c461942d1
jackykc <jkc1@ualberta.ca>
2018-01-27 14:52:39 -0700

    attempt at adding drf

---
+++ backend/backend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class memes(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    url = models.CharField(max_length=255, blank=False)
+
+
+class tags(models.Model):
+
+    name = models.CharField(max_length=255, blank=False)
+    probability = models.IntegerField()
+    meme = models.ForeignKey(memes)
