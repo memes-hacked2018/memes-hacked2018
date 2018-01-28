b0cd88d583aaa7b2877e4706d35eb70f4ba7d615
Chaitali Patel <chaitali_patel@intuit.com>
2018-01-28 04:17:51 -0700

    xd

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -71,14 +71,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'tempmonitor_db',
+#         'USER': 'prezatech',
+#         'PASSWORD': 'prezaadmin545',
+#         'HOST': 'tempmonitor-db.cii3csufs8jh.ca-central-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#     }
+# }
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'memez',
+        'USER': 'memeteam',
+        'PASSWORD': 'meme-team',
+        'HOST': 'meme-team.cvgevzl0ljet.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
