588ab3507f6dfa7542888379722623fad99ebd87
Hoeppner <david_hoeppner@intuit.com>
2018-01-27 21:35:30 -0700

    UI refactored

--- src/memes-front-end/App.js
+++ src/memes-front-end/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import Tabs from "./Tabs.js";
-import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+import { Root, Container, Header, Title, Button, Left, Right, Body, Icon, Footer } from 'native-base';
+import { Font, AppLoading } from "expo";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
   render() {
+    if (this.state.loading) {
+      return (
+        <Root>
+          <AppLoading />
+        </Root>
+      );
+    }
     return (
       <Container>
       <Header>
-        <Left>
-          <Button transparent>
-            <Icon name='menu' />
-          </Button>
-        </Left>
         <Body>
           <Title>Header</Title>
         </Body>
         <Right />
+        <Footer>
+          <Tabs/>
+        </Footer>
       </Header>
     </Container>
     );
--- src/memes-front-end/Tabs.js
+++ src/memes-front-end/Tabs.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
-export default class FooterTabsIconExample extends Component {
+
+export default class FooterTabs extends Component {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content />
-        <Footer>
-          <FooterTab>
-            <Button>
-              <Icon name="" />
-            </Button>
-            <Button>
-              <Icon name="camera" />
-            </Button>
-            <Button active>
-              <Icon active name="navigate" />
-            </Button>
-            <Button>
-              <Icon name="person" />
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+        <FooterTab>
+        <Button>
+            <Icon name="apps" />
+        </Button>
+        <Button>
+            <Icon name="camera" />
+        </Button>
+        <Button active>
+            <Icon active name="navigate" />
+        </Button>
+        <Button>
+            <Icon name="person" />
+        </Button>
+        </FooterTab>
     );
   }
 }
