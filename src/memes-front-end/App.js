0444f1e8dadff8ffabb9c70a30463f048d886054
Hoeppner <david_hoeppner@intuit.com>
2018-01-28 00:30:33 -0700

    IT FINALLY WORKS PRAISE THE BABY JESUS

--- src/memes-front-end/App.js
+++ src/memes-front-end/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import Tabs from "./Tabs.js";
-import { Root, Container, Header, Title, Button, Left, Right, Body, Icon, Footer } from 'native-base';
+import { Root, Container, Content, Header, Title, Button, Left, Right, Body, Icon, Footer, FooterTab } from 'native-base';
 import { Font, AppLoading } from "expo";
+import { StackNavigator, DrawerNavigator } from "react-navigation";
+// import Tabs from "./Tabs.js";
+
+import CreatePage from "./TabPages/CreatePage.js";
+import ProfilePage from "./TabPages/ProfilePage.js";
+import TagsPage from "./TabPages/TagsPage.js";
+
+// const ScreenNavigator = StackNavigator({
+//   Create: {
+//     screen: CreatePage,
+//     navigationOptions: {
+//       headerTitle: 'Create A Meme',
+//     },
+//   },
+//   Profile: {
+//     screen: ProfilePage,
+//     navigationOptions: {
+//       headerTitle: 'Profile',
+//     },
+//   },
+//   Tags: {
+//     screen: TagsPage,
+//     navigationOptions: {
+//       headerTitle: 'Tags',
+//     },
+//   }
+// })
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true };
+    this.state = { 
+      selectedTab: "default",
+      loading: true 
+    };
   }
 
   async componentWillMount() {
@@ -16,6 +45,22 @@ export default class App extends React.Component {
     });
     this.setState({ loading: false });
   }
+
+  renderSelectedTab () {
+    switch (this.state.selectedTab) {
+      case 'create':
+        return (<CreatePage />);
+        break;
+      case 'profile':
+        return (<ProfilePage />);
+        break;
+      case 'tags':
+        return (<TagsPage />);
+        break;
+      default:
+    }
+  }
+
   render() {
     if (this.state.loading) {
       return (
@@ -28,13 +73,28 @@ export default class App extends React.Component {
       <Container>
       <Header>
         <Body>
-          <Title>Header</Title>
+          <Title>Memes4Life</Title>
         </Body>
-        <Right />
-        <Footer>
-          <Tabs/>
-        </Footer>
       </Header>
+      <Content>
+        {this.renderSelectedTab()}
+      </Content>
+      <Footer>
+          <FooterTab>
+              <Button active={this.state.selectedTab==='create'} 
+               onPress={() => this.setState({selectedTab: 'create'})}>
+                  <Icon name="create" />
+              </Button>
+              <Button active={this.state.selectedTab==='profile'} 
+               onPress={() => this.setState({selectedTab: 'profile'})}>
+                  <Icon name="person" />
+              </Button>
+              <Button active={this.state.selectedTab==='tags'} 
+               onPress={() => this.setState({selectedTab: 'tags'})}>
+                  <Icon name="md-pricetags" />
+              </Button>
+          </FooterTab>
+        </Footer>
     </Container>
     );
   }
--- src/memes-front-end/Tabs.js
+++ src/memes-front-end/Tabs.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon } from 'native-base';
 
 export default class FooterTabs extends Component {
-  render() {
-    return (
-        <FooterTab>
-        <Button>
-            <Icon name="apps" />
-        </Button>
-        <Button>
-            <Icon name="camera" />
-        </Button>
-        <Button active>
-            <Icon active name="navigate" />
-        </Button>
-        <Button>
-            <Icon name="person" />
-        </Button>
-        </FooterTab>
-    );
-  }
+    render() {
+        return (
+            <Footer>
+                <FooterTab>
+                    <Button>
+                        <Icon name="apps" />
+                    </Button>
+                    <Button>
+                        <Icon name="camera" />
+                    </Button>
+                    <Button active>
+                        <Icon active name="navigate" />
+                    </Button>
+                    <Button>
+                        <Icon name="person" />
+                    </Button>
+                </FooterTab>
+            </Footer>
+        );
+    }
 }
