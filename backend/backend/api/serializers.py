75a5c28fe874944dedd418e52ff132f323ba863b
Chaitali Patel <chaitali_patel@intuit.com>
2018-01-27 20:32:58 -0700

    serializers work

--- backend/backend/api/serializers.py
+++ backend/backend/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Meme,Tag
 
 class MemeSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Meme
         fields = ('id', 'name', 'url')
@@ -12,18 +13,19 @@ class MemeSerializer(serializers.ModelSerializer):
         """
         return Meme.objects.create(**validated_data)
 
-
 class TagSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Tag
-        fields = ('id', 'name', 'probability', 'meme_id')
+        fields = ('id', 'name', 'probability', 'meme')
 
     def create(self, validated_data):
         """
         Create and return a new `Tag` instance, given the validated data.
         """
         return Tag.objects.create(**validated_data)
+
+
 
 
 
--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -6,6 +6,8 @@ from api import views
 urlpatterns = {
     url(r'^memes/$', views.MemeList.as_view()),
     url(r'^memes/(?P<pk>[0-9]+)/$', views.MemeDetail.as_view()),
+    url(r'^tags/$', views.TagList.as_view()),
+    url(r'^tags/(?P<pk>[0-9]+)/$', views.TagDetail.as_view()),
     # url(r'^memes/(?P<pk>[0-9]+)/$', DetailsMemesView.as_view(), name="details"),
     # url(r'^tags/$', CreateTagsView.as_view()),
     # url(r'^tags/(?P<pk>[0-9]+)/$', DetailsTagsView.as_view(), name="details"),
